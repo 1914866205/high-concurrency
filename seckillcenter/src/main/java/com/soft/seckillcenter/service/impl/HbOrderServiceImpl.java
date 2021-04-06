ce9f6c5fae0621658bdfb82dace2367bd683288c
ntt <1914866205@qq.com>
2021-04-06 16:04:13 +0800

    增加sentinel限流规则

--- seckillcenter/src/main/java/com/soft/seckillcenter/service/impl/HbOrderServiceImpl.java
+++ seckillcenter/src/main/java/com/soft/seckillcenter/service/impl/HbOrderServiceImpl.java
@@ -34,42 +34,29 @@ public class HbOrderServiceImpl implements HbOrderService {
 
     @Override
     public ResponseResult toSecKill(OrderDto orderDto) {
-        System.out.println(4);
 
         //1.redis先库存-1，要使用decrement方法，底层是线程安全的
         //返回的数据是减完之后的数字
         Long stock = redisTemplate.opsForValue().decrement(Constants.REDIS_PRODUCT_STOCK_PREFIX + orderDto.getPkGoodId());
         if (stock < 0) {
-            System.out.println(5);
-
             //如果库存＜0   防止少卖
             redisTemplate.opsForValue().increment(Constants.REDIS_PRODUCT_STOCK_PREFIX + orderDto.getPkGoodId());
             return ResponseResult.failure(ResultCode.GOOD_CLEAN);
         }
-
         //2.如果有剩余商品
         //发送消息给消息队列，传输用户id和订单信息，异步传输数据
         //秒杀活动每人限抢一个
         try {
-            System.out.println(6);
 
             //先把其他逻辑仅仅作为打印
             System.out.println("秒杀中心把订单发给消息中心");
             mqCenterFeignClient.messageToQueue(orderDto);
         } catch (Exception e) {
-            System.out.println(7);
-
             //如果遇到异常，回滚事务
             redisTemplate.opsForValue().increment(Constants.REDIS_PRODUCT_STOCK_PREFIX + orderDto.getPkGoodId());
             log.info("订单创建失败");
             return ResponseResult.failure(ResultCode.ORDER_CREATE_ERROR);
         }
-
-//        log.info("订单创建成功" + redisTemplate.opsForValue().get(Constants.REDIS_PRODUCT_STOCK_PREFIX + orderDto.getPkGoodId()));
-                     System.out.println(8);
         return ResponseResult.success();
-
-        //3.消息队列监听到数据变化，创建订单，此处先用RabbitMQ替代
-
     }
 }
